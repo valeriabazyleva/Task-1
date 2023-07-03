5d031336c0674fa5d4cf0693a14223148c5c1b3a
Valeria Bazyleva <valeryiabazyleva@gmail.com>
2023-07-03 21:30:36 +0300

    The solution of the problem

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,20 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        int rechargeAmount = 1100; // сумма пополнения
+        int balance = 100; // начальный баланс
+        double bonusAmount = 0;
+        if (rechargeAmount >= 1000){
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+            bonusAmount = (double) rechargeAmount / 100;
+
         }
+        double total = balance + rechargeAmount + bonusAmount;
+
+        System.out.println(bonusAmount + " начисленных бонусов");
+        System.out.println(" итоговая сумма на счету " + total);
+
+
     }
 }
